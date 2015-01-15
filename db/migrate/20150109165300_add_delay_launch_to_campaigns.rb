921e937cd4b0da89b54f86b6622d3b62317383e0
Mike Cooley <michael.cooley@cynergistek.com>
2015-01-15 10:48:17 -0600

    default launch_delay to false

--- db/migrate/20150109165300_add_delay_launch_to_campaigns.rb
+++ db/migrate/20150109165300_add_delay_launch_to_campaigns.rb
@@ -1,5 +1,5 @@
 class AddDelayLaunchToCampaigns < ActiveRecord::Migration
   def change
-    add_column :campaigns, :delay_launch, :boolean
+    add_column :campaigns, :delay_launch, :boolean, :default => false
   end
 end
